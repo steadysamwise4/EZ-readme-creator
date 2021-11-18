55bc16d92409e30dfabc35ca30bfabb00ec2f247
Sam Davenport <samueldavenport@att.net>
2021-11-17 18:42:47 -0600

    added multiple user questions

--- utils/questions.js
+++ utils/questions.js
@@ -6,8 +6,61 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
-        }
+            message: 'What is your name? (Required)'
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your github username? (Required)'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address: (Required)'
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the title of your project? (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please enter a detailed description of the project: (Required)'
+        },
+        {
+            type: 'input',
+            name: 'instructions',
+            message: 'Provide instructions for user installation:'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions for use:'
+        },
+        {
+            type: 'input',
+            name: 'altText',
+            message: 'Please enter alt text for the screen shot:'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please choose a license:',
+            choices: ['BSD', 'MIT', 'CC', 'APACHE']
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: 'Please list any references here:'
+        },
+        
     ]);
 };
 
